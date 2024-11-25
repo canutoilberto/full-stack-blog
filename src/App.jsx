ffb1c9b83274f4de4ad5af194e14c7ee2211f0c6
canutoilberto <canutoilberto@gmail.com>
2024-11-25 14:35:42 -0300

    [Setup] first steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 const App = () => {
   return (
-    <div className=''>Hello World</div>
-  )
-}
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      {/*NAVBAR*/}
+      {/*BREADCRUMB*/}
+      {/*INTRODUCTION*/}
+      {/*FEATURED POSTS*/}
+      {/*POST LIST*/}
+    </div>
+  );
+};
 
-export default App
+export default App;
